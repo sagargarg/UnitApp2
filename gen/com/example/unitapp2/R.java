b2327e1fde66673478861227761ed983720ca0f3
sagargarg <sagargarg@gmail.com>
2014-08-14 22:44:37 -0400

    Added buttons

--- gen/com/example/unitapp2/R.java
+++ gen/com/example/unitapp2/R.java
@@ -25,7 +25,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int FrameLayout1=0x7f080000;
+        public static final int Space1=0x7f080001;
+        public static final int Space2=0x7f080002;
+        public static final int Space3=0x7f080003;
+        public static final int action_settings=0x7f08000c;
+        public static final int distance=0x7f08000b;
+        public static final int ftpsec=0x7f080009;
+        public static final int mph=0x7f080008;
+        public static final int number=0x7f080006;
+        public static final int overview=0x7f080005;
+        public static final int time=0x7f08000a;
+        public static final int title=0x7f080004;
+        public static final int unit=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
