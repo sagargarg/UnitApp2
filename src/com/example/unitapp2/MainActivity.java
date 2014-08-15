2c3f6f9e52c0ef8e1052d5a62e86da6baf5c4999
sagargarg <sagargarg@gmail.com>
2014-08-14 23:40:25 -0400

    Get rid of some imports

--- src/com/example/unitapp2/MainActivity.java
+++ src/com/example/unitapp2/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.unitapp2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 
